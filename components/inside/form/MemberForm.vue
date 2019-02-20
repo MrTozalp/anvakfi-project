65257c0b9fea6f03866465f016347de1f477fc88
MrTozalp <ozlp.mrt@gmail.com>
2019-02-20 22:14:31 +0300

    Email kuralı geri alındı

--- components/inside/form/MemberForm.vue
+++ components/inside/form/MemberForm.vue
@@ -198,13 +198,8 @@ export default {
                 occupation: ""
             },
             rules: {
-                required: (value) => !!value || 'Zorunlu',
-                email: (value) => {
-                    if(value.length > 0){
-                        const pattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-                        return  pattern.test(value) || 'Geçersiz email';
-                    }
-                },
+                required: (v) => !!v || 'Zorunlu',
+                email: (v) =>  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Geçersiz email',
                 uniquePhone: (value) => {
                     return !this.modelList.some(el =>  ( el.phone === value && el.id !== this.member.id) )   || 'Bu telefon numarası ile kayıt bulunmaktadır. Lütfen başka bir numara giriniz.'
                 }
